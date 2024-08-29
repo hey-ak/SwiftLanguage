6d3d29fb5b380802c08b8b6eaf01761f0a4c925b
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-08-29 18:50:58 +0530

    Added Dictionaries in Swift

--- Dictionary.xcplaygroundpage/Contents.swift
+++ Dictionary.xcplaygroundpage/Contents.swift
@@ -13,5 +13,24 @@ print(myYoutubeVideos.values)// returns Array of Values
 
 var oldid = myYoutubeVideos.updateValue(135, forKey: "Flutter Course") // returns old value and updates new value in dictionary
 
-myYoutubeVideos["Flutter Course"] = nil // it removes key value pair from dictonary
+//myYoutubeVideos["Flutter Course"] = nil // it removes key value pair from dictonary
 print(myYoutubeVideos)
+myYoutubeVideos.removeValue(forKey: "Flutter Course") // this also works same like we did above
+print(myYoutubeVideos)
+
+
+let nestedDictionary = [
+    "user1": [
+        "name": "Akshay",
+        "age": "21"
+    ],
+    "user2": [
+        "name": "John",
+        "age": "25"
+    ]
+]
+
+var userTwoName = nestedDictionary["user2"]?["name"]
+print(userTwoName)
+
+
