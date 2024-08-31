66fbde23c91bb4786e2bb10bde3b1a17a933b1fe
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-08-31 17:00:50 +0530

    Structs Added In swift

---
+++ Struct.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+// Structures
+
+struct Marvel {
+    var series:String = " "
+    var aired:Int = 0
+    var network:String = " "
+   
+}
+
+var seriesDetails = Marvel(series: "Loki", aired: 2021, network: "Disney")
+print(seriesDetails.series)
+print(seriesDetails.aired)
+print(seriesDetails.network)
+
+
+var newSeries = Marvel()
+
+newSeries.series = "Wonda Vison"
+newSeries.aired = 2021
+newSeries.network = "Disney"
+
+
+print(newSeries.series)
+
+struct MarvelSeries {
+    var series:String = ""
+    var aired:Int = 0
+    var network:String = ""
+    
+    
+    func Details() -> String {
+        return("\n Series: \(series)\n Aired In: \(aired)\n Network: \(network)")
+    }
+}
+
+var details = MarvelSeries()
+details.series = "Ms. Marvel"
+details.aired = 2022
+details.network = "Disney"
+
+
+print(details.Details())
+
+
+struct MarvelProduction{
+    var series:String
+    var aired:Int
+    var network:String
+    
+    init(series: String, aired: Int, network: String) {
+        self.series = series
+        self.aired = aired
+        self.network = network
+    }
+}
+
+
+var series = MarvelProduction(series: "Hawkeye", aired: 2021, network: "Disney")
+
+print(series.aired)
